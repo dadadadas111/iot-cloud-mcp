24224a91028f45b8c04e48386f61f329d36cd24a
Nguyen Thanh Long <nguyenthanhlong12102004vp@gmail.com>
2026-02-24 11:10:54 +0700

    Update mcp.module.ts

--- src/mcp/mcp.module.ts
+++ src/mcp/mcp.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { McpService } from './services/mcp.service';
+import { AuthModule } from '@/auth/auth.module';
 import { ApiClientService } from '@/mcp/services/api-client.service';
 import { RedisService } from '@/mcp/services/redis.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthModule],
   providers: [McpService, ApiClientService, RedisService],
   exports: [McpService, ApiClientService, RedisService],
 })
